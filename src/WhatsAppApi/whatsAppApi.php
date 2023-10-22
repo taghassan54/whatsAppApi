8d685f7b3df7bb64d1a54ba66a8a792debf899a7
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2023-10-22 18:27:36 +0400

    update

--- src/WhatsAppApi/whatsAppApi.php
+++ src/WhatsAppApi/whatsAppApi.php
@@ -1,6 +1,6 @@
 <?php
  
- namespace WhatsAppApi\WhatsAppApi;
+ namespace WhatsAppApi\whatsAppApi;
 	
 	/**
 		* Class WhatsAppApi
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'taghassan54/whatsapp_api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '258f977a3658e9379c312b7c518973b9455404a7',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'taghassan54/whatsapp_api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '258f977a3658e9379c312b7c518973b9455404a7',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
