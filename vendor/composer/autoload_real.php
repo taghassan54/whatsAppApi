634af94b418988db28f8c51e4471da7d546c98f9
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2023-11-05 18:27:06 +0400

    update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit639407ede5da994144f0518fb6375f65
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit639407ede5da994144f0518fb6375f65::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
